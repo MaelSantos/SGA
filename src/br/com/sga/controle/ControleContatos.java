bf789c0ad21838c2a2f7f2f225a82ed9eeec7656
MaelSantos <maelsantos777@gmail.com>
2018-07-07 09:50:58 -0300

    0.1.4.5

--- src/br/com/sga/controle/ControleContatos.java
+++ src/br/com/sga/controle/ControleContatos.java
@@ -1,62 +1,73 @@
 package br.com.sga.controle;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import br.com.sga.entidade.Cliente;
 import br.com.sga.entidade.Telefone;
-import javafx.collections.FXCollections;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
-import javafx.scene.control.cell.TreeItemPropertyValueFactory;
+import javafx.util.Callback;
 
 public class ControleContatos implements Initializable{
 
-	 @FXML
-	    private TreeTableView<Cliente> tblContatos;
+	@FXML
+	private TreeTableView<String> tblContatos;
 
-	    @FXML
-	    private TreeTableColumn<Cliente, String> columnNome;
+	@FXML
+	private TreeTableColumn<String, String> columnNome;
 
-	    @FXML
-	    private TreeTableColumn<Cliente, String> columnTelefone;
+	@FXML
+	private TreeTableColumn<String, String> columnTelefone;
 
-	    @FXML
-	    private TreeTableColumn<Cliente, String> columnOpcoes;
+	@FXML
+	private TreeTableColumn<String, String> columnOpcoes;
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
-//		List<Cliente> clientes = tblContatos.setItems(FXCollections.observableArrayList(clientes));
-		
-//		empBoss.setNome("Mael");
-//		Cliente empSmith = new Cliente();
-//		Cliente empMcNeil = new Cliente();
-//		TreeItem<Cliente> itemRoot = new TreeItem<Cliente>(empBoss);
-//		TreeItem<Cliente> itemSmith = new TreeItem<Cliente>(empSmith);
-//		TreeItem<Cliente> itemMcNeil = new TreeItem<Cliente>(empMcNeil);
-//		itemRoot.getChildren().addAll(itemSmith, itemMcNeil);
-//		tblContatos.setRoot(itemRoot);
-
 		List<Telefone> list = Arrays.asList(new Telefone(1,1,1,new Cliente()));
 		Cliente cliente = new Cliente("Mael",list);
-		TreeItem<Cliente> item = new TreeItem<Cliente>(cliente);
 		
-		columnNome.setCellValueFactory(new TreeItemPropertyValueFactory<Cliente, String>("Nome"));
-
-	    columnTelefone.setCellValueFactory(new TreeItemPropertyValueFactory<Cliente, String>("Telefone"));
-
-	    columnOpcoes.setCellValueFactory(new TreeItemPropertyValueFactory<Cliente, String>("Opções"));
+		TreeItem<String> itemNome = new TreeItem<String>(cliente.getNome());	
 		
-	
-        
-        
+		List<TreeItem<String>> treeItems = new ArrayList<>();
+		for(Telefone t : list)
+			treeItems.add(new TreeItem<String>("("+t.getPrefixo()+") "+t.getNumero()));
+		
+		TreeItem<String> itemTelefones = new TreeItem<String>();
+		itemTelefones.getChildren().setAll(treeItems);
+		
+		TreeItem<String> root = new TreeItem<String>();
+		root.getChildren().addAll(itemNome,itemTelefones);
+		
+		
+		columnNome.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<String,String>, ObservableValue<String>>() {
+			@Override
+			public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<String, String> param) {
+				return new SimpleStringProperty(param.getValue().getValue());
+			}
+		});
+			
+		columnTelefone.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<String,String>, ObservableValue<String>>() {			
+			@Override
+			public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<String, String> param) {
+				return new SimpleStringProperty(param.getValue().getValue());
+			}
+		});
+		
+		
+		tblContatos.setRoot(root);
+		
 	}
 
 
