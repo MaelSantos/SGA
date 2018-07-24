1a428edcc30d963e496ec0ba17dd5768030cac64
MaelSantos <maelsantos777@gmail.com>
2018-07-24 00:56:51 -0300

    0.4.0

--- src/br/com/sga/controle/ControleProcesso.java
+++ src/br/com/sga/controle/ControleProcesso.java
@@ -16,7 +16,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.MouseEvent;
 
 public class ControleProcesso extends Controle{
 
