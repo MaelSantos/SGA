1fe17515f6bb6544a019eda51ffd6b8139bba8dc
wanderson100v <wanderson.lira@ufrpe.br>
2018-08-10 17:17:52 -0300

    0.6.8
    Adicao de gráfico pizza para receitas e despesas por mes (Estatisticas)

--- src/br/com/sga/controle/ControleEstatistica.java
+++ src/br/com/sga/controle/ControleEstatistica.java
@@ -22,14 +22,18 @@ import br.com.sga.fachada.IFachada;
 import br.com.sga.view.Alerta;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import model.Pessoa;
 
 public class ControleEstatistica extends Controle{
 
@@ -63,6 +67,11 @@ public class ControleEstatistica extends Controle{
     @FXML
     private BarChart<String, Number> barChart;
     
+    private PieChart piechart;
+   
+    @FXML
+    private GridPane graficosPane;
+    
     @FXML
     private Label descricaoLabel;
     
@@ -92,14 +101,21 @@ public class ControleEstatistica extends Controle{
 	    	    	try {
 		    	    	if(graficoBox.getValue() == TipoGrafico.BARRA) 
 		    	    	{
+		    	    		barChart.setTitle(tipoBox.getValue().toString() + " de "+ de.toString() +" até "+ ate.toString());
 		    	    		if(tipoBox.getValue() == TipoEstatistica.RECEITAS_POR_MES) 
 		    		    		gerarGraficoBarra(fachada.buscarContaTotalMesPorIntervalo(de, ate,Tabela.RECEITA),Tabela.RECEITA);
 		    	    		else if(tipoBox.getValue() == TipoEstatistica.DESPESAS_POR_MES) 
 		    	    			gerarGraficoBarra(fachada.buscarContaTotalMesPorIntervalo(de, ate,Tabela.DESPESAS),Tabela.DESPESAS);
-		    	    		
-		    	    		log = new Log(new Date(System.currentTimeMillis()), EventoLog.GERAR, funcionario.getNome(), "Gerar Grafico: "
-		    	    				+graficoBox.getValue()+" - "+tipoBox.getValue(), StatusLog.COLCLUIDO);
+		    	    	}else if(graficoBox.getValue() == TipoGrafico.PIZZA) {
+		    	    		piechart.setTitle(tipoBox.getValue().toString() + " de "+ de.toString() +" até "+ ate.toString());
+		    	    		if(tipoBox.getValue() == TipoEstatistica.RECEITAS_POR_MES) 
+		    		    		gerarGraficoPizza(fachada.buscarContaTotalMesPorIntervalo(de, ate,Tabela.RECEITA),Tabela.RECEITA);
+		    	    		else if(tipoBox.getValue() == TipoEstatistica.DESPESAS_POR_MES) 
+		    	    			gerarGraficoPizza(fachada.buscarContaTotalMesPorIntervalo(de, ate,Tabela.DESPESAS),Tabela.DESPESAS);
 		    	    	}
+		    	    	
+		    	    	log = new Log(new Date(System.currentTimeMillis()), EventoLog.GERAR, funcionario.getNome(), "Gerar Grafico: "
+	    	    				+graficoBox.getValue()+" - "+tipoBox.getValue(), StatusLog.COLCLUIDO);
 		    	    	
 	    	    	} catch (Exception e) {
     					Alerta.getInstance().showMensagem("Erro","",e.getMessage());
@@ -119,6 +135,12 @@ public class ControleEstatistica extends Controle{
 	    	
     	}else if(event.getSource() == tipoBox) {
     		descricaoLabel.setText(tipoBox.getSelectionModel().getSelectedItem().toString().toLowerCase());
+    	}else if(event.getSource() == graficoBox) {
+    		if(graficoBox.getSelectionModel().getSelectedItem() == TipoGrafico.BARRA) {
+    			graficosPane.getChildren().setAll(barChart);
+    		}else if(graficoBox.getSelectionModel().getSelectedItem() == TipoGrafico.PIZZA) {
+    			graficosPane.getChildren().setAll(piechart);
+    		}
     	}
 	    	
     }
@@ -137,6 +159,15 @@ public class ControleEstatistica extends Controle{
     	barChart.getData().add(p);
     }
     
+    private void gerarGraficoPizza(List<ContaAdapter> contas,Tabela tabela) {
+    	piechart.getData().clear();
+    	
+    	Calendar c = Calendar.getInstance();
+    	for(ContaAdapter conta: contas) { 
+			piechart.getData().add(new PieChart.Data(c.get(Calendar.MONTH)+"/"+c.get(Calendar.YEAR),conta.getValorTotal()));
+    	}
+    }
+    
 	@Override
 	public void atualizar(Tela tela, Object object) {
 		
@@ -152,5 +183,6 @@ public class ControleEstatistica extends Controle{
         graficoBox.getItems().addAll(TipoGrafico.values());
         tipoBox.getItems().addAll(TipoEstatistica.values());
         graficoBox.setValue(TipoGrafico.BARRA);
+        piechart = new PieChart();
 	}
 }
